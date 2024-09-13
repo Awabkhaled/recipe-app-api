c3aa5cf314a498257de5cd077b8443e7c9ec0331
Awab Khaled <khaledawab3@gmail.com>
2024-09-13 19:34:08 +0300

    testing unit test in django(modifying2)

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,13 +4,12 @@ a simple test
 from django.test import SimpleTestCase
 from . import calc
 
+
 class calcTest(SimpleTestCase):
-    """
-    test calc methos
-    """
+    """test calc method"""
+
     def test_plus(self):
         element1 = 5
         element2 = 5
-        res=calc.plus(element1, element2)
+        res = calc.plus(element1, element2)
         self.assertEqual(res, 10)
-        
