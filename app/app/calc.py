32f7bfb6e074a4da34a7616a243822fc8b25a869
Awab Khaled <khaledawab3@gmail.com>
2024-09-13 23:30:25 +0300

    testing unit test in django(modifying2)

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,2 +1,5 @@
 def plus(number1, number2):
     return number1 + number2
+
+def sub(number1, number2):
+    return number1 - number2
--- app/app/tests.py
+++ app/app/tests.py
@@ -13,3 +13,9 @@ class calcTest(SimpleTestCase):
         element2 = 5
         res = calc.plus(element1, element2)
         self.assertEqual(res, 10)
+
+    def test_sub(self):
+        element1 = 10
+        element2 = 2
+        res=calc.sub(element1, element2)
+        self.assertEqual(res,8)
